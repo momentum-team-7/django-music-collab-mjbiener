e9511b83057735ef7e4c995d5ebf1637bd5a2701
mjbiener <michaelbiener@yahoo.com>
2021-03-02 10:13:37 -0500

    fixed the integrity error all working now

--- core/models.py
+++ core/models.py
@@ -4,24 +4,25 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
 class Home(models.Model):
     pass
 
-class Album(models.Model):
-    title = models.CharField(max_length=280)
-    artist = models.CharField(max_length=280)
-    release_year = models.IntegerField(blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="artists")
-
-    def __str__(self):
-        return f"{self.title}, {self.artist}, {self.release_year}"
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
-
 
     def __str__(self):
         return self.name
 
 
+class Album(models.Model):
+    title = models.CharField(max_length=280)
+    
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_year = models.IntegerField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="albums")
+    
+
+    def __str__(self):
+        return f"{self.title}, {self.artist}, {self.release_year}"
